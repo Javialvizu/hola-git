51435a34c049dd995e01026ea0d8750abe336848
Javialvizu <javialvizugar@gmail.com>
2025-12-20 22:47:05 +0000

    Primer commir codigo de python

---
+++ HELLO_PHYTON.py
@@ -0,0 +1,2 @@
+print("Hola esta es mi primera vez programando en python")
+print("Me gustaria agregar que la verdad no se nada absolutamente nada de este lenguaje de programacion, pero imagino es similar a JAVA")
