f3b17ef45387825d9bc9fac18db9531c265e7555
Javialvizu <javialvizugar@gmail.com>
2025-12-21 20:04:56 +0000

    Practica de variables finalizada

---
+++ variables.py
@@ -0,0 +1,8 @@
+nombre = "Javier"
+edad = 21
+altura = 1.67
+aprendiendo_python = True
+print("Nombre =", nombre)
+print("Edad =", edad)
+print("Altura =", altura)
+print("Esta aprendiendo python?", aprendiendo_python)
