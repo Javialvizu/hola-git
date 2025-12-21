5922ff24ddecf6eba749285201fd0c8f1012d2ab
Javialvizu <javialvizugar@gmail.com>
2025-12-21 19:55:17 +0000

    Esta es la poresnetacion e inicio de la practica en python

---
+++ presentacion.py
@@ -0,0 +1,4 @@
+nombre = input ("Ingrese su nombre:")
+edad = input("Ingrese edad:")
+comida = input("Ingrese comida favorita:")
+print("Hola hoy presentamos a ", nombre,"nigga", "Este chimpanse tiene", edad, "anios y le gusta comer ",comida)
