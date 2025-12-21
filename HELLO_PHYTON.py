1674c85d239a1e4e1fd1de4f283a55412d2483a8
Javialvizu <javialvizugar@gmail.com>
2025-12-21 18:20:37 +0000

    Commit de eliminacion

--- HELLO_PHYTON.py
+++
@@ -1,2 +0,0 @@
-print("Hola esta es mi primera vez programando en python")
-print("Me gustaria agregar que la verdad no se nada absolutamente nada de este lenguaje de programacion, pero imagino es similar a JAVA")
