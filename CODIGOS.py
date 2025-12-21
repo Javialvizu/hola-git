2ac617a4c26c4837b5be4c39b9ec14c2238f05c3
Javialvizu <javialvizugar@gmail.com>
2025-12-21 19:46:49 +0000

    Primereas lineas de codigo formales

--- CODIGOS.py
+++ CODIGOS.py
@@ -1,1 +1,4 @@
-print("HOLA COMO ESTAS MIS NIGGAS")
+nombre = input ("Ingrese su nombre:")
+edad = input("Ingrese edad:")
+comida = input("Ingrese comida favorita:")
+print("Hola hoy presentamos a ", nombre,"nigga", "Este chimpanse tiene", edad, "anios y le gusta comer ",comida)
