52ef824025d4c86cb091c1a22187dbc9391d70b8
Javialvizu <javialvizugar@gmail.com>
2025-12-22 23:46:41 +0000

    Deleted file

--- variables.py
+++
@@ -1,8 +0,0 @@
-nombre = "Javier"
-edad = 21
-altura = 1.67
-aprendiendo_python = True
-print("Nombre =", nombre)
-print("Edad =", edad)
-print("Altura =", altura)
-print("Esta aprendiendo python?", aprendiendo_python)
