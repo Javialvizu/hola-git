7cd79409eb714d293afaece0873b25b64d930529
Javialvizu <javialvizugar@gmail.com>
2026-01-29 20:25:12 -0600

    Comnezamos con las condiocionales if y else if

---
+++ PRACTICAS/CONDICIONALES/if-else.py
@@ -0,0 +1,20 @@
+#Ejemplo de estructura condicional if-else
+""""
+edad = int(input("Ingrese su edad:"))
+
+if edad >=18:
+    print("Eres mayor de edad")
+
+else:
+    print("Eres menor de edad")
+"""
+#Ejemplo de estructura condicional if-elif-else
+
+contraseña_almacenada = "Javialvizu77"
+contraseña = input("Ingrese su contraseña:")
+
+if contraseña == contraseña_almacenada:
+    print ("ACCESO CONCEDIDO")
+
+else:
+    print("ACCESO DENEGADO")
