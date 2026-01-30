6c25063f5542a920b98f1d7ae07ddab14cd20e7b
Javialvizu <javialvizugar@gmail.com>
2026-01-30 14:21:59 -0600

    Se realizo mv hacia la carpeta OPERADORES

---
+++ PRACTICAS/OPERADORES/operadores-logicos.py
@@ -0,0 +1,30 @@
+#AND
+resultado1 = True and True  # True
+resultado2 = True and False  # False
+resultado3 = False and True  # False
+resultado4 = False and False  # False
+
+#OR
+resultado5 = True or True  # True
+resultado6 = True or False  # True
+resultado7 = False or True  # True
+resultado8 = False or False  # False
+
+#NOT
+resultado9 = not True  # False
+resultado10 = not False  # True 
+
+resultado11 = not 20 > 10  # False 
+
+print(resultado1)
+print(resultado2)
+print(resultado3)
+print(resultado4)
+print(resultado5)
+print(resultado6)
+print(resultado7)
+print(resultado8)
+print(resultado9)
+print(resultado10)
+print(resultado11)
+
