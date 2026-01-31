d4ea19569672eeaaa49e1a4cb42a6dadd3f5fdce
Javialvizu <javialvizugar@gmail.com>
2026-01-31 13:48:20 -0600

    Ejercicio nuevo

---
+++ PRACTICAS/EJERCICIOS/general/mayor-menor-igual.py
@@ -0,0 +1,9 @@
+numero1 = int(input("Ingresa el primer número: "))
+numero2 = int(input("Ingresa el segundo número: ")) 
+
+if numero1 > numero2:
+    print(f"El numeero {numero1} es mayor a {numero2}")
+elif numero1 < numero2:
+    print(f"El número {numero2} es mayor a {numero1}")
+else:
+    print("Ambos números son iguales")  
