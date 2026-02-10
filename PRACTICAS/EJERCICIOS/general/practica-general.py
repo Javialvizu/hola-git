675439f8e768f2d78863f38b0214bb2d2b0dfeb6
Javialvizu <javialvizugar@gmail.com>
2026-02-10 16:29:31 -0600

    Parte 1 de ejercicio general

--- PRACTICAS/EJERCICIOS/general/practica-general.py
+++ PRACTICAS/EJERCICIOS/general/practica-general.py
@@ -1,7 +1,33 @@
-def registrar_estudiante():
+"""def registrar_estudiante():
     condicional = "0"
+    lista_estudiantes = []
     while condicional != "salir":
-        input
-        print("ola")
-
+        nombre = input("Ingrese su nombre: ")
+        apellido = input("Ingrese su apellido: ")
+        edad = input("Ingrese su edad: ")
+        carrera = input("Ingrese su carrera: ")
+        lista_estudiantes.append((nombre,apellido,edad,carrera))
+        
         condicional = input("Desea salir? ingrese (salir): ")
+        condicional = condicional.lower()
+
+    return (lista_estudiantes)
+
+lista_estudiante = registrar_estudiante()
+print(lista_estudiante)"""
+
+def registrar_alumnos():
+    condicional = "0"
+    lista_alumnos = []
+    while condicional != "salir":
+        cadena = input("Ingrese nombre,apellido,edad,carrera: ")
+        cadena = cadena.split(",")
+        lista_alumnos.append(cadena)
+
+        condicional = input("terminar (salir)" )
+        condicional = condicional.lower()
+
+    return(lista_alumnos)
+
+alumnos = registrar_alumnos()
+print(alumnos)
