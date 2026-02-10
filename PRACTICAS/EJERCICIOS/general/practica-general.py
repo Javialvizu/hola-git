e786d90b4dcf283b99960f1ad0d87e1aa9924022
Javialvizu <javialvizugar@gmail.com>
2026-02-09 21:44:14 -0600

    Ultimo commit de la noche

---
+++ PRACTICAS/EJERCICIOS/general/practica-general.py
@@ -0,0 +1,7 @@
+def registrar_estudiante():
+    condicional = "0"
+    while condicional != "salir":
+        input
+        print("ola")
+
+        condicional = input("Desea salir? ingrese (salir): ")
